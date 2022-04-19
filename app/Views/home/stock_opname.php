b55149ae42d8f0f4c4c8550482c6450aacd2fff9
Edoleonardo <edoleonardo198@gmail.com>
2022-04-19 10:29:27 +0700

    asd

--- app/Views/home/stock_opname.php
+++ app/Views/home/stock_opname.php
@@ -336,16 +336,11 @@
 
 </footer>
 <script type="text/javascript">
-    let html5QrcodeScanner = new Html5QrcodeScanner(
+    var html5QrcodeScanner = new Html5QrcodeScanner(
         "reader", {
             fps: 10,
-            qrbox: {
-                width: 250,
-                height: 250
-            }
-        },
-        /* verbose= */
-        false);
+            qrbox: 250
+        });
 
     function matiinscan() {
         html5QrcodeScanner.clear();
